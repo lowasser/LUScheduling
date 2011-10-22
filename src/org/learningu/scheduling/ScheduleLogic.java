331e79336a52e8d7b8a2dcb361a7ce224e8bd1e7
Louis Wasserman <wasserman.louis@gmail.com>
2011-10-21 23:08:54 -0500

    Fix imports

--- src/org/learningu/scheduling/ScheduleLogic.java
+++ src/org/learningu/scheduling/ScheduleLogic.java
@@ -1,20 +1,10 @@
 package org.learningu.scheduling;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-import org.learningu.scheduling.graph.Course;
-import org.learningu.scheduling.graph.Room;
-import org.learningu.scheduling.graph.Teacher;
-import org.learningu.scheduling.graph.TimeBlock;
 import org.learningu.scheduling.util.Condition;
 
-import com.google.common.collect.Table;
-import com.google.common.collect.Table.Cell;
 import com.google.inject.ImplementedBy;
 
 @ImplementedBy(DefaultScheduleLogic.class)
 public interface ScheduleLogic {
-
   Condition isValid(Schedule schedule);
 }
