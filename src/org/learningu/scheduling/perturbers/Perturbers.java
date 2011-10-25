aa2644fa2ac6e0c6ea1e809ea74241057b555ea8
Louis Wasserman <wasserman.louis@gmail.com>
2011-10-25 17:23:52 -0500

    Add deserialization for perturbers.

---
+++ src/org/learningu/scheduling/perturbers/Perturbers.java
@@ -0,0 +1,57 @@
+package org.learningu.scheduling.perturbers;
+
+import java.util.Random;
+
+import org.learningu.scheduling.optimization.Perturber;
+import org.learningu.scheduling.perturbers.SerialPerturbers.ScaledPerturber;
+import org.learningu.scheduling.perturbers.SerialPerturbers.SequencedPerturber;
+import org.learningu.scheduling.perturbers.SerialPerturbers.SerialPerturberImpl;
+import org.learningu.scheduling.schedule.Schedule;
+
+import com.google.common.collect.ImmutableList;
+
+public final class Perturbers {
+  private Perturbers() {
+  }
+
+  public static Perturber<Schedule> deserialize(SequencedPerturber serial) {
+    ImmutableList.Builder<Perturber<Schedule>> sequenceBuilder = ImmutableList.builder();
+    for (ScaledPerturber seq : serial.getPerturbList()) {
+      sequenceBuilder.add(deserialize(seq));
+    }
+    final ImmutableList<Perturber<Schedule>> sequence = sequenceBuilder.build();
+    return new Perturber<Schedule>() {
+      @Override
+      public Schedule perturb(Schedule initial, double temperature) {
+        Schedule current = initial;
+        for (Perturber<Schedule> seq : sequence) {
+          current = seq.perturb(current, temperature);
+        }
+        return current;
+      }
+    };
+  }
+
+  private static Perturber<Schedule> deserialize(ScaledPerturber serial) {
+    final Perturber<Schedule> delegate = deserialize(serial.getImpl());
+    final double tempScale = serial.getTemperatureScale();
+    return new Perturber<Schedule>() {
+      @Override
+      public Schedule perturb(Schedule initial, double temperature) {
+        return delegate.perturb(initial, temperature * tempScale);
+      }
+    };
+  }
+
+  private static Perturber<Schedule> deserialize(SerialPerturberImpl serial) {
+    Random random = new Random();
+    switch (serial) {
+      case DESTRUCTIVE:
+        return new DestructivePerturber(random);
+      case GREEDY:
+        return new GreedyPerturber(random);
+      default:
+        throw new AssertionError();
+    }
+  }
+}
--- src/org/learningu/scheduling/scorers/Scorers.java
+++ src/org/learningu/scheduling/scorers/Scorers.java
@@ -1,23 +1,16 @@
 package org.learningu.scheduling.scorers;
 
-import junit.framework.AssertionFailedError;
-
 import org.learningu.scheduling.optimization.Scorer;
 import org.learningu.scheduling.schedule.Schedule;
 import org.learningu.scheduling.scorers.SerialScorers.CompleteScorer;
 import org.learningu.scheduling.scorers.SerialScorers.ScaledScorer;
 import org.learningu.scheduling.scorers.SerialScorers.SerialScorerImpl;
 
-import com.google.common.base.Throwables;
 import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableMap;
 
 public final class Scorers {
-  private static final ImmutableMap<SerialScorerImpl, Class<? extends Scorer<Schedule>>> impls =
-      ImmutableMap.<SerialScorerImpl, Class<? extends Scorer<Schedule>>> builder()
-          .put(SerialScorerImpl.DISTINCT_CLASSES, DistinctClassesScorer.class)
-          .put(SerialScorerImpl.EST_STUDENT_HOURS, EstimatedStudentHoursScorer.class)
-          .build();
+  private Scorers() {
+  }
 
   public static Scorer<Schedule> deserialize(CompleteScorer serial) {
     ImmutableList.Builder<Scorer<Schedule>> componentsBuilder = ImmutableList.builder();
@@ -51,16 +44,13 @@ public final class Scorers {
   }
 
   private static Scorer<Schedule> deserialize(SerialScorerImpl serial) {
-    Class<? extends Scorer<Schedule>> impl = impls.get(serial);
-    if (impl == null) {
-      throw new AssertionFailedError("Do not know how to construct " + serial.name());
-    }
-    try {
-      return impl.newInstance();
-    } catch (InstantiationException e) {
-      throw Throwables.propagate(e);
-    } catch (IllegalAccessException e) {
-      throw Throwables.propagate(e);
+    switch (serial) {
+      case DISTINCT_CLASSES:
+        return new DistinctClassesScorer();
+      case EST_STUDENT_HOURS:
+        return new EstimatedStudentHoursScorer();
+      default:
+        throw new AssertionError();
     }
   }
 
