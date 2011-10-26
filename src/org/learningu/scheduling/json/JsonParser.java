4a3c1fadb8cd2525ffffc0448fe166c4b5b5039f
Louis Wasserman <wasserman.louis@gmail.com>
2011-10-26 13:08:49 -0500

    Prepare to move to using GSON

--- src/org/learningu/scheduling/json/JsonParser.java
+++
@@ -1,8 +0,0 @@
-package org.learningu.scheduling.json;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-abstract class JsonParser<T> {
-  public abstract T parseJsonToProto(JSONObject object) throws JSONException;
-}
