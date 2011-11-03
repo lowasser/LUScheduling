674ad0f87a7cb433c6d33e9637b28f05916a2569
Louis Wasserman <wasserman.louis@gmail.com>
2011-11-03 14:16:18 -0500

    Delete some gratuitous logic in RoomConflictLogic

--- src/org/learningu/scheduling/logic/RoomConflictLogic.java
+++ src/org/learningu/scheduling/logic/RoomConflictLogic.java
@@ -1,16 +1,7 @@
 package org.learningu.scheduling.logic;
 
-import static com.google.common.base.Preconditions.checkArgument;
-
-import com.google.common.collect.DiscreteDomains;
-import com.google.common.collect.Range;
-import com.google.common.collect.Ranges;
-
-import java.util.List;
-
 import org.learningu.scheduling.schedule.PresentAssignment;
 import org.learningu.scheduling.schedule.Schedule;
-import org.learningu.scheduling.schedule.StartAssignment;
 
 /**
  * Logic for verifying that the assignment would not conflict with some other class.
@@ -28,23 +19,5 @@ public final class RoomConflictLogic extends ScheduleLogic {
         assignment,
         schedule.occurringAt(assignment.getPeriod(), assignment.getRoom()).asSet(),
         "Classes may not use the same room at the same time");
-  }
-
-  private boolean overlaps(StartAssignment assign1, StartAssignment assign2) {
-    Range<Integer> range1 = periodIndexRange(assign1);
-    Range<Integer> range2 = periodIndexRange(assign2);
-    return !assign1.getTimeBlock().equals(assign2.getTimeBlock()) || !range1.isConnected(range2)
-        || !range1.intersection(range2).canonical(DiscreteDomains.integers()).isEmpty();
-  }
-
-  private static Range<Integer> periodIndexRange(StartAssignment assign) {
-    int start = assign.getPeriod().getIndex();
-    int length = assign.getSection().getPeriodLength();
-    return Ranges.closedOpen(start, start + length);
-  }
-
-  private static <E> E getLast(List<E> list) {
-    checkArgument(!list.isEmpty());
-    return list.get(list.size() - 1);
   }
 }
